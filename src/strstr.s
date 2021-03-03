b9b844e4012f3180d99490c7bc414654964b83ec
Corentin Mas <corentinmas@yahoo.com>
2021-03-03 15:57:41 +0100

    [Fix] strstr pointer fix

--- src/strstr.s
+++ src/strstr.s
@@ -8,7 +8,6 @@ strstr:
     mov rax, 0
     jmp strstr_loop
 
-
 strstr_loop:
     mov cl, BYTE [rdi]
     cmp cl, 0
@@ -17,20 +16,21 @@ strstr_loop:
     mov r9, rsi
     cmp cl, BYTE [rsi]
     je strstr_cmp
+    jmp strstr_inc
+
+strstr_inc:
     inc rdi
-    inc rsi
     jmp strstr_loop
 
-
 strstr_cmp:
-    mov cl, BYTE [r8]
+    mov cl, BYTE [r9]
     cmp cl, 0
     je strstr_end
     cmp cl, BYTE [r9]
-    jne strstr_loop
+    jne strstr_inc
     inc r8
     inc r9
-
+    jmp strstr_cmp
 
 strstr_end:
     mov rax, rdi
