e035607a484b7095d96481eb4f6853c6bb312da1
Corentin Mas <corentinmas@yahoo.com>
2021-03-03 15:08:24 +0100

    [Fix] Memcpy range

--- src/memcpy.s
+++ src/memcpy.s
@@ -5,21 +5,17 @@ SECTION .text
 GLOBAL memcpy
 
 memcpy:
-    mov r8, rdi ; cpy ptr dest
-    mov r9, rsi ; cpy ptr src
-    mov r10, rdx ; cpy size
-    mov rdi, 1 ; reset rdi to 0 to use as incr
     jmp memcpy_loop
 
 memcpy_loop:
-    mov cl, BYTE [r9]
-    mov [r8], cl ; cpy byte
-    cmp r10, rdi; compare to size
-    jne memcpy_inc
-    ret
-
-memcpy_inc:
-    inc rdi ; increment
-    inc r8 ; mv dest
-    inc r9 ; mv src
+    cmp rdx, 0; compare to size
+    je memcpy_end
+    mov cl, BYTE [rsi]
+    mov [rdi], cl ; cpy byte
+    dec rdx
+    inc rdi ; mv dest
+    inc rsi ; mv src
     jmp memcpy_loop;
+
+memcpy_end:
+    ret
